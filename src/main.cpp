c67ae1c82a3d80920ccfb18197f1875b018b1644
herbglitch <herbglitch@protonmail.com>
2022-11-04 19:18:22 -0600

    update to cmake packages

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
 #include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_render.h>
+#include <SDL_surface.h>
 #include <stdio.h>
 
 #define SCREEN_WIDTH 1280 
@@ -22,6 +25,27 @@ int main(int argc, char** argv){
         return 1;
     }
 
+    SDL_Surface *logoSurface = IMG_Load("res/logo.png");
+    if(!logoSurface){
+        printf("Error: Failed to load image\nSDL_Image Error: '%s'\n", IMG_GetError());
+        return 1;
+    }
+    
+    SDL_Texture *logo = SDL_CreateTextureFromSurface(renderer, logoSurface);
+    if(!logo){
+        printf("Error: Failed to create texture\n");
+        return 1;
+    }
+
+    SDL_Rect logoDst = {
+        .x = (SCREEN_WIDTH  / 2) - 200, //logo with 400 / 2
+        .y = (SCREEN_HEIGHT / 2) - 200, //logo height 400 / 2
+        .w = 400,                       //logo width
+        .h = 400                        //logo height
+    };
+
+    SDL_FreeSurface(logoSurface);
+
     bool running = true;
     while(running){
         SDL_Event event;
@@ -36,14 +60,18 @@ int main(int argc, char** argv){
             }
         }
 
-        SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
+        SDL_SetRenderDrawColor(renderer, 0x0c, 0x1c, 0x2c, 0x00);
         SDL_RenderClear(renderer);
+
+        SDL_RenderCopy(renderer, logo, NULL, &logoDst);
 
         SDL_RenderPresent(renderer);
     }
 
+    SDL_DestroyTexture(logo);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+    IMG_Quit();
 
     return 0;
 }
